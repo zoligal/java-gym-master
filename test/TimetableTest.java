e8f1e1635aea7047e9ff6617acbb0831bc9c4f82
zoligal <zoligal@yandex.ru>
2026-07-31 21:54:54 +0300

    Add missing imports to TimetableTest for CI

--- test/TimetableTest.java
+++ test/TimetableTest.java
@@ -3,6 +3,14 @@
 import ru.yandex.practicum.gym.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.gym.model.TrainingSession;
+import ru.yandex.practicum.gym.model.CounterOfTrainings;
+import ru.yandex.practicum.gym.service.Timetable;
+import ru.yandex.practicum.gym.model.DayOfWeek;
+import ru.yandex.practicum.gym.model.TimeOfDay;
+import ru.yandex.practicum.gym.model.Group;
+import ru.yandex.practicum.gym.model.Coach;
+
 
 import java.util.List;
 
